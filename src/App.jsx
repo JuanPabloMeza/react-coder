36742a4f4174bdce965639a2a203caf17c506cb8
juampi <juampiimeza@gmail.com>
2024-10-10 17:25:03 -0300

    navbar 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navbar } from "./components/layout/navbar/navbar";
+import { Home } from "./components/pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Car colection</h1>
-  )
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layout/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import "./navbar.css";
+
+export const Navbar = () => {
+  return (
+    <>
+        <div className="container-navbar">
+            <h2>car colection</h2>
+            <ul>
+                <li>todos</li>
+                <li>0km</li>
+                <li>usados</li>
+                <li>deluxe</li>
+            </ul>
+        </div>
+    </>
+  );
+};
